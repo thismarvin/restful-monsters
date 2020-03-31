65076b75e07c71c18dd70f968a68354d70e7453b
thismarvin <bend_@outlook.com>
2020-03-30 23:56:14 -0500

    laid out levels route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoute = require("./routes/user.js");
+const levelRoute = require("./routes/level.js");
 
 const HOST = '0.0.0.0';
 const PORT = 9090;
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use("/api", userRoute);
+app.use("/api", levelRoute);
 
 app.get('/', (req, res) => {
     res.send("Hello World");
