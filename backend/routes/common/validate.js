500107314423b20e8019eb321af4c6b45c475714
thismarvin <bend_@outlook.com>
2020-04-10 21:46:01 -0500

    added user validation and more endpoints

--- backend/routes/common/validate.js
+++ backend/routes/common/validate.js
@@ -1,1 +1,59 @@
-// TODO: add common request body param validation here. ಥ_ಥ
+const bcrypt = require("bcrypt");
+
+function validateUsername(username) {
+    const validated = username.toString().trim();
+
+    if (validated.length === 0) {
+        throw {
+            "code": "USERNAME_TOO_SHORT",
+            "message": "A valid username must be at least one character long.",
+            "validationError": true
+        };
+    }
+    if (validated.length > 32) {
+        throw {
+            "code": "USERNAME_TOO_LONG",
+            "message": "A valid username must be at least one character long.",
+            "validationError": true
+        };
+    }
+
+    if (/[^\w-.]/g.test(validated)) {
+        throw {
+            "code": "USERNAME_INVALID_CHARACTERS",
+            "message": "A valid username must consist of only alphanumeric characters, with the exception of an underscore, dash, and period.",
+            "validationError": true
+        };
+    }
+
+    return validated;
+}
+
+function validatePassword(password) {
+    const validated = password.toString().trim();
+
+    if (validated.length < 8) {
+        throw {
+            "code": "PASSWORD_TOO_SHORT",
+            "message": "A valid password must be at least eight characters long.",
+            "validationError": true
+        };
+    }
+
+    return validated;
+}
+
+async function validateUser(user) {
+    const username = validateUsername(user.username);
+    const password = validatePassword(user.password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    return {
+        "username": username,
+        "hashedPassword": hashedPassword
+    };
+}
+
+module.exports = {
+    validateUser
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,59 +1,132 @@
 const express = require("express");
 const errors = require("./common/errors.js");
+const validation = require("./common/validate.js");
 const db = require("../database/setup.js").jocose;
 
 const router = express.Router();
 
-// Gets all registered users.
-router.get("/users", async (_, res) => {
-    try {
-        const result = await db.query(`SELECT * FROM users ORDER BY id;`);
-        res.status(200).json(result);
-    } catch (error) {
-        switch (error.code) {
-            default:
-                errors.handle(res, error);
-                break;
-        }
-    }
-});
-
-// Gets a user with a specific login.
-router.get("/users/:login", async (req, res) => {
-    try {
-        const result = await db.query(`SELECT * FROM users WHERE login="${req.body.login}";`);
-
-        if (result.length === 0) {
-            errors.sendNotFound(res, "Could not find a user with that login.");
-            return;
-        }
-
-        res.status(200).json(result);
-    } catch (error) {
-        switch (error.code) {
-            default:
-                errors.handle(res, error);
-                break;
-        }
-    }
-});
-
+//#region CRUD
 // Creates a user.
 router.post("/users", async (req, res) => {
-    if (!req.body.login) {
-        errors.sendBadRequest(res, "Invalid request body.");
+    if (!req.body.username || !req.body.password) {
+        errors.sendBadRequest(res, `Invalid request body. Expected 'username' and 'password' keys and values.`);
         return;
     }
 
     try {
-        await db.query(`INSERT INTO users (login) VALUES ("${req.body.login}");`);
-        const result = await db.query(`SELECT * FROM users WHERE login="${req.body.login}";`);
-        res.status(201).json(result);
+        const user = {
+            "username": req.body.username,
+            "password": req.body.password
+        };
+
+        const validatedUser = await validation.validateUser(user);
+        const insertUserResponse = await db.query(`INSERT INTO users (login, password, created_at) VALUES ("${validatedUser.username}", "${validatedUser.hashedPassword}", CURRENT_TIMESTAMP());`);
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE id="${insertUserResponse.insertId}";`);
+        res.status(201).json(selectUserResponse);
     } catch (error) {
+        if (error.validationError) {
+            errors.sendBadRequest(res, error.message);
+            return;
+        }
+
         switch (error.code) {
             case "ER_DUP_ENTRY":
                 errors.sendConflict(res, "A user with that login already exists.");
                 break;
+            default:
+                errors.handle(res, error);
+                break;
+        }
+    }
+});
+
+// Gets a user with a specific username.
+router.get("/users/:username", async (req, res) => {
+    try {
+        const username = req.params.username.toString().trim();
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${username}";`);
+
+        if (selectUserResponse.length === 0) {
+            errors.sendNotFound(res, "Could not find a user with that username.");
+            return;
+        }
+
+        res.status(200).json(selectUserResponse[0]);
+    } catch (error) {
+        switch (error.code) {
+            default:
+                errors.handle(res, error);
+                break;
+        }
+    }
+});
+
+// Updates everything about a user.
+router.put("/users/:username", async (req, res) => {
+    // TODO: Implement
+});
+
+// Updates a specific value of a user.
+router.patch("/users/:username", async (req, res) => {
+    // TODO: Implement
+});
+
+// Deletes a user.
+router.delete("/users/:username", async (req, res) => {
+    try {
+        const username = req.params.username.toString();
+        const selectUserResponse = await db.query(`SELECT id FROM users WHERE login="${username}";`);
+
+        if (selectUserResponse.length === 0) {
+            errors.sendNotFound(res, "Could not find a user with that username.");
+            return;
+        }
+
+        const userID = selectUserResponse[0].id;
+
+        await db.query(`DELETE FROM users WHERE id=${userID};`);
+        res.status(204).json();
+    } catch (error) {
+        switch (error.code) {
+            default:
+                errors.handle(res, error);
+                break;
+        }
+    }
+});
+//#endregion
+
+// Gets all registered users.
+router.get("/users", async (_, res) => {
+    try {
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users ORDER BY id;`);
+        res.status(200).json(selectUserResponse);
+    } catch (error) {
+        switch (error.code) {
+            default:
+                errors.handle(res, error);
+                break;
+        }
+    }
+});
+
+// Gets all levels from a specific user.
+router.get("/users/:username/levels", async (req, res) => {
+    try {
+        const username = req.params.username.toString().trim();
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${username}";`);
+
+        if (selectUserResponse.length === 0) {
+            errors.sendNotFound(res, "Could not find a user with that username.");
+            return;
+        }
+
+        const userID = selectUserResponse[0].id;
+        const selectLevelsResponse = await db.query(`SELECT id, name, description, data, created_at FROM levels WHERE user_id = ${userID};`);
+
+        res.status(200).json(selectLevelsResponse);
+    } catch (error) {
+        switch (error.code) {
             default:
                 errors.handle(res, error);
                 break;
