d106ef93dce012c5c20df53e721c3dc8540d2529
thismarvin <bend_@outlook.com>
2020-04-13 20:42:34 -0500

    refactored user validation & some SQL queries

--- backend/routes/common/validate.js
+++ backend/routes/common/validate.js
@@ -1,65 +1,91 @@
 const bcrypt = require("bcrypt");
 
-function validateUsername(username) {
-    const validated = username.toString().trim();
-
-    if (validated.length === 0) {
-        throw {
-            "code": "USERNAME_TOO_SHORT",
-            "message": "A valid username must be at least one character long.",
-            "validationError": true
-        };
-    }
-    if (validated.length > 32) {
-        throw {
-            "code": "USERNAME_TOO_LONG",
-            "message": "A valid username must be at least one character long.",
-            "validationError": true
-        };
+class UserValidator {
+    constructor() {
+        this.saltRounds = 10;
     }
 
-    if (/[^\w-.]/g.test(validated)) {
-        throw {
-            "code": "USERNAME_INVALID_CHARACTERS",
-            "message": "A valid username must consist of only alphanumeric characters, with the exception of an underscore, dash, and period.",
-            "validationError": true
-        };
+    /**
+     * Formats and validates a given username, and returns the result.
+     * @param {String} username The username to format and validate.
+     */
+    processUsername(username) {
+        const formattedUsername = this.formatUsername(username);
+
+        if (this.validateUsername(formattedUsername)) {
+            return formattedUsername;
+        }
     }
 
-    return validated;
-}
+    async processPassword(password) {
+        const formattedPassword = this.formatPassword(password);
 
-function validatePassword(password) {
-    const validated = password.toString().trim();
-
-    if (validated.length < 8) {
-        throw {
-            "code": "PASSWORD_TOO_SHORT",
-            "message": "A valid password must be at least eight characters long.",
-            "validationError": true
-        };
+        if (this.validatePassword(formattedPassword)) {
+            return await bcrypt.hash(password, this.saltRounds);
+        }
     }
 
-    return validated;
+    /**
+     * Formats a given username, and returns the result as a String.
+     * @param {String} username The username to format.
+     * @private
+     */
+    formatUsername(username) {
+        return username.toString().trim();
+    }
+
+    /**
+     * Tests whether or not a given username is valid. Throws an error otherwise.
+     * @param {String} username A formatted username to validate.
+     * @private
+     */
+    validateUsername(username) {
+        if (username.length === 0) {
+            throw {
+                "code": "USERNAME_TOO_SHORT",
+                "message": "A valid username must be at least one character long.",
+                "validationError": true
+            };
+        }
+        if (username.length > 32) {
+            throw {
+                "code": "USERNAME_TOO_LONG",
+                "message": "A valid username must be at least one character long.",
+                "validationError": true
+            };
+        }
+
+        if (/[^\w-.]/g.test(username)) {
+            throw {
+                "code": "USERNAME_INVALID_CHARACTERS",
+                "message": "A valid username must consist of only alphanumeric characters, with the exception of an underscore, dash, and period.",
+                "validationError": true
+            };
+        }
+
+        return true;
+    }
+
+    formatPassword(password) {
+        return password.toString().trim();
+    }
+
+    validatePassword(password) {
+        if (password.length < 8) {
+            throw {
+                "code": "PASSWORD_TOO_SHORT",
+                "message": "A valid password must be at least eight characters long.",
+                "validationError": true
+            };
+        }
+
+        return true;
+    }
 }
 
-async function hashPassword(password) {
-    return await bcrypt.hash(password, 10);
-}
-
-async function validateUser(user) {
-    const username = validateUsername(user.username);
-    const password = validatePassword(user.password);
-    const hashedPassword = hashPassword(password);
-
-    return {
-        "username": username,
-        "hashedPassword": hashedPassword
-    };
-}
+const userValidater = new UserValidator();
 
 module.exports = {
-    validateUsername,
-    hashPassword,
-    validateUser
+    processUsername: userValidater.processUsername.bind(userValidater),
+    processPassword: userValidater.processPassword.bind(userValidater)
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -14,13 +14,10 @@ router.post("/users", async (req, res) => {
     }
 
     try {
-        const user = {
-            "username": req.body.username,
-            "password": req.body.password
-        };
+        const username = validation.processUsername(req.body.username);
+        const password = await validation.processPassword(req.body.password);
 
-        const validatedUser = await validation.validateUser(user);
-        const insertUserResponse = await db.query(`INSERT INTO users (login, password, created_at) VALUES ("${validatedUser.username}", "${validatedUser.hashedPassword}", CURRENT_TIMESTAMP());`);
+        const insertUserResponse = await db.query(`INSERT INTO users (login, password, created_at) VALUES ("${username}", "${password}", CURRENT_TIMESTAMP());`);
         const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE id="${insertUserResponse.insertId}";`);
         res.status(201).json(selectUserResponse);
     } catch (error) {
@@ -43,8 +40,7 @@ router.post("/users", async (req, res) => {
 // Gets a user with a specific username.
 router.get("/users/:username", async (req, res) => {
     try {
-        const username = req.params.username.toString().trim();
-        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${username}";`);
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${req.params.username}";`);
 
         if (selectUserResponse.length === 0) {
             errors.sendNotFound(res, "Could not find a user with that username.");
@@ -69,21 +65,18 @@ router.put("/users/:username", async (req, res) => {
     }
 
     try {
-        const user = {
-            "username": req.body.username,
-            "password": req.body.password
-        };
-        const validatedUser = await validation.validateUser(user);
+        const username = validation.processUsername(req.body.username);
+        const password = await validation.processPassword(req.body.password);
 
-        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${validatedUser.username}";`);
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${req.params.username}";`);
 
         if (selectUserResponse.length === 0) {
-            const insertUserResponse = await db.query(`INSERT INTO users (login, password, created_at) VALUES ("${validatedUser.username}", "${validatedUser.hashedPassword}", CURRENT_TIMESTAMP());`);
+            const insertUserResponse = await db.query(`INSERT INTO users (login, password, created_at) VALUES ("${username}", "${password}", CURRENT_TIMESTAMP());`);
             const selectCreatedUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE id="${insertUserResponse.insertId}";`);
             res.status(201).json(selectCreatedUserResponse[0]);
         } else {
             const userID = selectUserResponse[0].id;
-            await db.query(`UPDATE users SET login="${validatedUser.username}", password="${validatedUser.hashedPassword}" WHERE id = ${userID};`);
+            await db.query(`UPDATE users SET login="${username}", password="${password}" WHERE id = ${userID};`);
             const selectUpdatedUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE id=${userID};`);
             res.status(200).json(selectUpdatedUserResponse[0]);
         }
@@ -123,23 +116,21 @@ router.patch("/users/:username", async (req, res) => {
 
         let username = null;
         let password = null;
-        let modifications = "";
+        let modifications = [];
 
         if (req.body.username) {
-            username = validation.validateUsername(req.body.username);
-            modifications += `login="${username}"`;
+            username = validation.processUsername(req.body.username);
+            modifications.push(`login="${username}"`);
         }
 
         if (req.body.password) {
-            password = await validation.hashPassword(req.body.password);
-            modifications += modifications.length > 0 ? ", " : "";
-            modifications += `password="${password}"`;
+            password = await validation.processPassword(req.body.password);
+            modifications.push(`password="${password}"`);
         }
 
-        await db.query(`UPDATE users SET ${modifications} WHERE id = ${userID}`);
+        await db.query(`UPDATE users SET ${modifications.toString()} WHERE id = ${userID}`);
         const selectUpdatedUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE id=${userID};`);
         res.status(200).json(selectUpdatedUserResponse[0]);
-
     } catch (error) {
         if (error.validationError) {
             errors.sendBadRequest(res, error.message);
@@ -160,8 +151,7 @@ router.patch("/users/:username", async (req, res) => {
 // Deletes a user.
 router.delete("/users/:username", async (req, res) => {
     try {
-        const username = req.params.username.toString();
-        const selectUserResponse = await db.query(`SELECT id FROM users WHERE login="${username}";`);
+        const selectUserResponse = await db.query(`SELECT id FROM users WHERE login="${req.params.username}";`);
 
         if (selectUserResponse.length === 0) {
             errors.sendNotFound(res, "Could not find a user with that username.");
@@ -199,8 +189,7 @@ router.get("/users", async (_, res) => {
 // Gets all levels from a specific user.
 router.get("/users/:username/levels", async (req, res) => {
     try {
-        const username = req.params.username.toString().trim();
-        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${username}";`);
+        const selectUserResponse = await db.query(`SELECT id, login, created_at FROM users WHERE login="${req.params.username}";`);
 
         if (selectUserResponse.length === 0) {
             errors.sendNotFound(res, "Could not find a user with that username.");
